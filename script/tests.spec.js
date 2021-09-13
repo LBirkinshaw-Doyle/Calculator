73e265c51a84f65814a2cbe9a0530905ac630eef
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2021-09-13 13:06:20 +0100

    new file to automate tests

---
+++ script/tests.spec.js
@@ -0,0 +1,21 @@
+const calculator = require('./calculation');
+
+describe('add', () => {
+	test('adds 0 and 0', () => {
+		expect(calculator.add(0,0)).toBe(0);
+	});
+
+	test('adds 2 and 2', () => {
+		expect(calculator.add(2,2)).toBe(4);
+	});
+
+	test('adds positive numbers', () => {
+		expect(calculator.add(2,6)).toBe(8);
+	});
+});
+
+describe('subtract', () => {
+	test('subtracts numbers', () => {
+		expect(calculator.subtract(10,4)).toBe(6);
+	});
+});
