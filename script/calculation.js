5107572ebde7cb22fce62977c6803597f90f6756
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2021-09-22 10:51:33 +0100

    added button click functionality

--- script/calculation.js
+++ script/calculation.js
@@ -1,4 +1,4 @@
-let displatString = "";
+let displayString = "";
 let calculationString = "";
 let numberButtonArray = [];
 let operatorButtonArray = [];
@@ -6,3 +6,36 @@ let operatorButtonArray = [];
 numberButtonArray = document.querySelectorAll(".number");
 operatorButtonArray = document.querySelectorAll(".operator");
 const display = document.querySelector("#display");
+
+numberButtonArray.forEach((element) => element.addEventListener("click", parseInput));
+operatorButtonArray.forEach((element) => element.addEventListener("click", parseInput));
+
+function parseInput (click) {
+    let button = click.target;
+    let input
+    button.firstChild? input = String(button.firstChild.textContent) : input = String(button.textContent);
+    
+    displayString += input; 
+    
+    switch (input) {
+        case operatorButtonArray[0].firstChild.textContent:
+            calculationString += " * ";
+        break;
+        case operatorButtonArray[1].firstChild.textContent:
+            calculationString += " / ";
+        break;
+        case operatorButtonArray[2].firstChild.textContent:
+            calculationString += " - ";
+        break;
+        case operatorButtonArray[3].firstChild.textContent:
+            calculationString += " + ";
+        break;
+        default:
+            calculationString += input;
+        break;
+    }
+
+    display.textContent = displayString;
+    display.classList.remove("blink");
+    console.log(displayString, calculationString);
+}
