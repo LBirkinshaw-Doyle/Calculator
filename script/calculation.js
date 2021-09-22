0488175e3b0bf2dc6ea57d879ea1149f6ce66b95
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2021-09-22 09:15:10 +0100

    added start of js file

---
+++ script/calculation.js
@@ -0,0 +1,8 @@
+let displatString = "";
+let calculationString = "";
+let numberButtonArray = [];
+let operatorButtonArray = [];
+
+numberButtonArray = document.querySelectorAll(".number");
+operatorButtonArray = document.querySelectorAll(".operator");
+const display = document.querySelector("#display");
